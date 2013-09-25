97ae8cb5639d228b2c3b0a1b0d2cbe1c2b2cd1d4
Alan Barber <abarber@alanbarber.org>
2013-09-24 20:22:14 -0400

    First revision of ExampleApp done and updated nuget packages for solution

--- TwoFactorNet.ExampleApp/Form1.Designer.cs
+++ TwoFactorNet.ExampleApp/Form1.Designer.cs
@@ -28,23 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pbQRCode = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblSecretKey = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.pbCurrentVerificationCode = new System.Windows.Forms.ProgressBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblCurrentVerificationCode = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbQRCode)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pbQRCode
+            // 
+            this.pbQRCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbQRCode.Location = new System.Drawing.Point(3, 16);
+            this.pbQRCode.Name = "pbQRCode";
+            this.pbQRCode.Size = new System.Drawing.Size(269, 256);
+            this.pbQRCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQRCode.TabIndex = 0;
+            this.pbQRCode.TabStop = false;
+            this.pbQRCode.MouseHover += new System.EventHandler(this.pbQRCode_MouseHover);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbQRCode);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 275);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "QRCode";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblSecretKey);
+            this.groupBox2.Location = new System.Drawing.Point(307, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(275, 48);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Secret Key";
+            // 
+            // lblSecretKey
+            // 
+            this.lblSecretKey.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblSecretKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecretKey.Location = new System.Drawing.Point(3, 16);
+            this.lblSecretKey.Name = "lblSecretKey";
+            this.lblSecretKey.Size = new System.Drawing.Size(269, 29);
+            this.lblSecretKey.TabIndex = 0;
+            this.lblSecretKey.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(307, 206);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(275, 81);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // pbCurrentVerificationCode
+            // 
+            this.pbCurrentVerificationCode.Location = new System.Drawing.Point(6, 66);
+            this.pbCurrentVerificationCode.Maximum = 30;
+            this.pbCurrentVerificationCode.Name = "pbCurrentVerificationCode";
+            this.pbCurrentVerificationCode.Size = new System.Drawing.Size(263, 23);
+            this.pbCurrentVerificationCode.Step = 1;
+            this.pbCurrentVerificationCode.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblCurrentVerificationCode);
+            this.groupBox3.Controls.Add(this.pbCurrentVerificationCode);
+            this.groupBox3.Location = new System.Drawing.Point(307, 84);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(275, 95);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Current Verification Code";
+            // 
+            // lblCurrentVerificationCode
+            // 
+            this.lblCurrentVerificationCode.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblCurrentVerificationCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentVerificationCode.Location = new System.Drawing.Point(3, 16);
+            this.lblCurrentVerificationCode.Name = "lblCurrentVerificationCode";
+            this.lblCurrentVerificationCode.Size = new System.Drawing.Size(269, 47);
+            this.lblCurrentVerificationCode.TabIndex = 5;
+            this.lblCurrentVerificationCode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 489);
+            this.ClientSize = new System.Drawing.Size(601, 306);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "TwoFactor.Net Example App";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQRCode)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbQRCode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblSecretKey;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ProgressBar pbCurrentVerificationCode;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblCurrentVerificationCode;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- TwoFactorNet.ExampleApp/Form1.cs
+++ TwoFactorNet.ExampleApp/Form1.cs
@@ -1,20 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
 
 namespace TwoFactorNet.ExampleApp
 {
     public partial class Form1 : Form
     {
+        private byte[] _secretKey;
+        private string _encodedKey;
+        private DateTime _nextCycleTime;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // get a 16 character random string that is base32
+            // get rnd 20 byte array
+            _encodedKey = System.Text.Encoding.ASCII.GetString(GenerateRandomKey());
+            _secretKey = Base32.Decode(_encodedKey);
+            // display
+            lblSecretKey.Text = string.Format("{0} {1} {2} {3}",
+                _encodedKey.Substring(0, 4), _encodedKey.Substring(4, 4),
+                _encodedKey.Substring(8, 4), _encodedKey.Substring(12, 4));
+            // get qrcode
+            IBarcodeWriter barcodeWriter = new BarcodeWriter
+            {
+                                               Format = BarcodeFormat.QR_CODE,
+                                               Options = new QrCodeEncodingOptions
+                                               {
+                                                   Width = 250,
+                                                   Height = 250,
+                                                   Margin = 1
+                                               }
+                                           };
+            pbQRCode.Image = new Bitmap(barcodeWriter.Write(GenerateQRCodeText("TwoFactorNet", "TestUser", _encodedKey)));
+
+            // generate code
+            var totp = new Totp(_secretKey, 6, 30);
+            lblCurrentVerificationCode.Text = totp.GeneratePassword(GetUnixTime());
+
+            // get next cycle time
+            _nextCycleTime = GetNextCycleTime();
+
+            // move progressbar to correct value
+            pbCurrentVerificationCode.Value = (_nextCycleTime.Second == 0 ? 60 - DateTime.Now.Second : DateTime.Now.Second);
+
+            // setup timer to display
+            timer.Enabled = true;
+        }
+
+        string GenerateQRCodeText(string issuer, string accountName, string secret)
+        {
+            return String.Format("otpauth://totp/{0}:{1}?secret={2}&digits=6&issuer={0}", issuer, accountName, secret);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // update progress bar
+            pbCurrentVerificationCode.PerformStep();
+
+            // check time to see if we need to cycle
+            if (DateTime.Now >= _nextCycleTime)
+            {
+                _nextCycleTime = GetNextCycleTime();
+                var totp = new Totp(_secretKey, 6, 30);
+                lblCurrentVerificationCode.Text = totp.GeneratePassword(GetUnixTime());
+                pbCurrentVerificationCode.Value = 0;
+                
+            }
+        }
+
+        private byte[] GenerateRandomKey()
+        {   
+            var retBytes = new byte[16];
+            for (int i = 0; i < retBytes.Length; i++)
+            {
+                retBytes[i] = GetValidBase32Byte();
+            }
+            return retBytes;
+        }
+
+        private byte GetValidBase32Byte()
+        {
+            var validBytes = System.Text.Encoding.ASCII.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567");
+            var rngByte = new byte[1];
+            var rngCsp = new RNGCryptoServiceProvider();
+            while (!Array.Exists(validBytes, e => e == rngByte[0]))
+            {
+                rngCsp.GetBytes(rngByte);    
+            }
+            return rngByte[0];
+        }
+
+        static DateTime GetNextCycleTime()
+        {
+            return DateTime.Now.AddSeconds(-DateTime.Now.Second).AddSeconds(DateTime.Now.Second < 30 ? 30 : 60);
+        }
+
+        long GetUnixTime()
+        {
+            return (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+        }
+
+        private void pbQRCode_MouseHover(object sender, EventArgs e)
+        {
+            var tt = new ToolTip();
+            tt.SetToolTip((Control)sender, GenerateQRCodeText("TwoFactorNet", "TestUser", _encodedKey));
+
         }
     }
 }
